e8f9ffee18dc0f96601c7d9183985fc5a2e2bb63
A08019DIRP_C2C.02.02 <A08019DIRP_C2C.02.02@accenture.com>
2019-04-17 14:53:04 +0530

    push

--- maven/src/main/resources/mav.java
+++ maven/src/main/resources/mav.java
@@ -1,6 +1,6 @@
 
-public class mav {
-public static void main(String args){
-	System.out.println("arfs";)
-}
-}
+//public class mav {
+//public static void main(String args){
+//	System.out.println("arfs";)
+//}
+//}
